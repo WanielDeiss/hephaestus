a9b517f7bf39e80d71f390f76d91073d73c1bd90
Daniel Weiß <wanieldeiss@gmail.com>
2020-03-02 05:13:52 +0100

    add basic health check for nest

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TerminusModule } from '@nestjs/terminus';
+import { TerminusOptionsService } from './terminus-options.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TerminusModule.forRootAsync({
+      useClass: TerminusOptionsService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
