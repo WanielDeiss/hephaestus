46614ad16ec9d728f1a991821cdfe2bdf8960501
Daniel Weiß <wanieldeiss@gmail.com>
2020-03-04 05:15:05 +0100

    add config module and dotenv file

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TerminusModule } from '@nestjs/terminus';
 import { TerminusOptionsService } from './terminus-options.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({}),
     TerminusModule.forRootAsync({
       useClass: TerminusOptionsService,
     }),
