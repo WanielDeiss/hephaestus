83ce102a7686c30d0f3f06b882af5f7454b3c6b3
Daniel Weiß <wanieldeiss@gmail.com>
2020-03-15 15:22:22 +0100

    deactivate terminus

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
-import { TerminusModule } from '@nestjs/terminus';
+// import { TerminusModule } from '@nestjs/terminus';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TerminusOptionsService } from './terminus-options.service';
+// import { TerminusOptionsService } from './terminus-options.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({}),
-    TerminusModule.forRootAsync({
-      useClass: TerminusOptionsService,
-    }),
+    // TerminusModule.forRootAsync({
+    //   useClass: TerminusOptionsService,
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
